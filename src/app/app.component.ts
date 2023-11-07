a4c3c29106bae05be22381e6dd232c6ad883b228
tommygaessler <tommy.gaessler@zoom.us>
2023-11-07 13:45:17 -0700

    uploading sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import uitoolkit from "@zoom/videosdk-ui-toolkit";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'uikits-in-angular';
+
+  UIToolKitConfig: any = {
+    features: ['video', 'audio', 'share', 'chat', 'settings', 'users']
+  }
+
+  constructor(public httpClient: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.getVideoSDKJWT()
+  }
+
+  getVideoSDKJWT() {
+    this.UIToolKitConfig.userName = 'test'
+    this.UIToolKitConfig.sessionName = 'hello'
+    this.UIToolKitConfig.sessionPasscode = 'test'
+
+    this.httpClient.post('https://or116ttpz8.execute-api.us-west-1.amazonaws.com/default/videosdk', JSON.stringify({
+	    sessionName: this.UIToolKitConfig.sessionName,
+	    role: 1
+    })).toPromise().then((data: any) => {
+      if(data.signature) {
+        console.log(data.signature)
+        this.UIToolKitConfig.videoSDKJWT = data.signature
+        this.joinSession()
+      } else {
+        console.log(data)
+      }
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+  joinSession() {
+
+    var sessionContainer = document.getElementById('sessionContainer') as HTMLElement
+
+    uitoolkit.joinSession(sessionContainer, this.UIToolKitConfig)
+
+    uitoolkit.onSessionJoined((data: any) => {
+      console.log(data)
+      console.log('session joined')
+    })
+
+    uitoolkit.onSessionClosed((data: any) => {
+      console.log(data)
+      console.log('session closed')
+    })
+  }
 }
